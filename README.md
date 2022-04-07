58e4892c32d0e989ea1a5113851bb4e43826cb68
Aleksandr Voronovich <vsanya93@mail.ru>
2022-04-07 18:40:56 +0300

    commit

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/uu9ddsjssh56j8h1/branch/master?svg=true)](https://ci.appveyor.com/project/valex182/selenium-selenide/branch/master)
+
+# Домашнее задание к занятию "2.1. Тестирование веб-интерфейсов"
+## Задание 1. Заказ карты
+### Воронович А.А.
+
+Ссылка на задание: https://github.com/netology-code/aqa-homeworks/tree/master/web
---
+++ src/test/java/CardOrderTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CardOrderTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver","./driver/windows/chromedriver.exe");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void shouldSendForm() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Васильев Дмитрий");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79222881908");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String actualText = driver.findElement(By.cssSelector(".Success_successBlock__2L3Cw")).getText().trim();
+        String expectedText = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expectedText, actualText);
+    }
+}
